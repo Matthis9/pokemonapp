33ee488f0e8c63b837d0b1b12501ccaecafc151e
Matthis9 <ti.math97@gmail.com>
2021-11-02 00:11:43 +0100

    Travail de Matthis Louis-sidney

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from "./components/layouts/NavBar";
-import Dashboard from "./components/layouts/Dashboard";
+
+import './App.css';
+
+
+
+import NavBar from './components/layouts/NavBar';
+import Dashboard from './components/layouts/Dashboard';
+import Pokemon from './components/pokemon/Pokemon';
 
 class App extends Component {
-render() {
-  return (
-    <div className="App">
-      <NavBar /><div className="container"><Dashboard></Dashboard></div>
-      
-      </div>
-  );
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <NavBar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route exact path="/pokemon/:pokemonIndex" component={Pokemon} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
   }
 }
 
